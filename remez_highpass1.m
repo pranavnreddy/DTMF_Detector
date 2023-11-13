d715817ca27f1c64c3f3b8f964518b4ea5a7df24
pmaddire <74743711+pmaddire@users.noreply.github.com>
2023-11-12 18:29:41 -0800

    Add files via upload

---
+++ remez_highpass1.m
@@ -0,0 +1,32 @@
+function [filtered_signal] = remez_highpass1(z,fs)
+
+% Filter specifications
+Fs = fs;             % Sampling frequency
+stopband_freq = 950;    % Stopband frequency in Hz
+N = 22;                % Filter order
+
+% Normalized frequencies
+normalized_stopband_freq = stopband_freq / (Fs / 2);
+
+% Frequency vector for the filter design
+Frequencies = [0, normalized_stopband_freq, normalized_stopband_freq + 0.1, 1];
+
+% Desired amplitude response
+Amplitudes = [0, 1, 1, 1];
+
+% Weights for the filter design
+Weights = [1, 5];
+
+% Filter design using Remez algorithm
+b_highpass = remez(N, Frequencies, Amplitudes, Weights);
+
+filtered_signal = filter(b_highpass, 1, z);
+
+% Plot the frequency response
+%freqz(b_highpass, 1, 1024, Fs);
+title('Frequency Response of Remez FIR Highpass Filter');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+grid on;
+
+end
---
+++ remez_lowpass1.m
@@ -0,0 +1,32 @@
+function [filtered_signal] = remez_lowpass1(z,fs)
+
+% Filter specifications
+Fs = fs;             % Sampling frequency
+Fc = 1650;              % Cutoff frequency in Hz
+N = 22;                % Filter order
+
+% Normalized frequencies
+normalizedFc = Fc / (Fs / 2);
+
+% Frequency vector for the filter design
+Frequencies = [0, normalizedFc, normalizedFc+0.1, 1];
+
+% Desired amplitude response
+Amplitudes = [1, 1, 0, 0];
+
+% Weights for the filter design
+Weights = [1, 5];
+
+% Filter design using Remez algorithm
+b = remez(N, Frequencies, Amplitudes, Weights);
+
+filtered_signal = filter(b,1,z);
+
+% Plot the frequency response
+%freqz(b, 1, 1024, Fs);
+title('Frequency Response of Remez FIR Lowpass Filter');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+grid on;
+
+end
